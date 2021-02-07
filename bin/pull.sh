f6f853721409704acd76c2544b4174b82ca4ca83
Karol Trybalski <trybalski.karol@gmail.com>
2021-02-07 19:47:15 +0100

    fixed fit pulling commands;

--- bin/pull.sh
+++ bin/pull.sh
@@ -11,8 +11,8 @@ check_repos_changes
 
 info "---> Pulling API <---"
 git -C $(api_path) checkout master
-git -C $(api_path) pull
+git -C $(api_path) pull origin master
 
 info "---> Pulling WEBAPP <---"
 git -C $(webapp_path) checkout master
-git -C $(webapp_path) pull
+git -C $(webapp_path) pull origin master
